87a9b71f8be0985c792f8507e453beb62d9d8147
Nadira3 <paitanun35@gmail.com>
2023-04-23 14:30:38 +0100

    main.h upload

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#define BUF_SIZE 1024
+int _printf(const char *format, ...);
+#endif
