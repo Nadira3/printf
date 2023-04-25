2128ba85eab4eee8ef58489455f4317688fc182e
Nadira3 <paitanun35@gmail.com>
2023-04-25 16:11:32 -0700

    int main(void)

---
+++ binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_div - converts a number to base 2 by recursive split
+ * @n: number
+ */
+void binary_div(int n)
+{
+	if (n / 2)
+		binary_div(n / 2);
+	_putchar((n % 2) + '0');
+}
+void negative_binary_div(int n)
+{
+	if (n / 2)
+		negative_binary_div(n / 2);
+	_putchar(-((n % 2) - 1) + '0');
+}
+/**
+ * print_binary - prints integer
+ * @list: variable list
+ */
+void print_binary(va_list list)
+{
+	int n = va_arg(list, int);
+	int flag = n > 0 ? 0 : 1;
+	
+	n = n > 0 ? n : -n;
+	if (!flag)
+		binary_div(n);
+	else
+		negative_binary_div(n);
+}
