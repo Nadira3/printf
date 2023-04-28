6d01bb92a907748880192921da0383b364cfa2c8
Nadira3 <paitanun35@gmail.com>
2023-04-28 06:41:51 -0700

    main.h update: increased the buffer size

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
-#define BUFSIZE 1024
+#define BUFSIZE 2048
 
 /**
  * struct format - a template for the format specifiers and corresponding
